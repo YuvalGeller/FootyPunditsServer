6e0ab0c643c076d81c3ab23c70c0a72ccd1a753b
Yuval Geller <User@LAB3-12>
2021-11-15 14:20:52 +0200

    Added Login

--- FootyPundits/Controllers/MainController.cs
+++ FootyPundits/Controllers/MainController.cs
@@ -45,6 +45,31 @@ namespace FootyPundits.Controllers
             }
         }
 
+        [Route("Login")]
+        [HttpGet]
+        public UserAccount Login([FromQuery] string email, [FromQuery] string pass)
+        {
+            UserAccount user = context.Login(email, pass);
+
+            //Check user name and password
+            if (user != null)
+            {
+                HttpContext.Session.SetObject("theUser", user);
+
+                Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+
+                //Important! Due to the Lazy Loading, the user will be returned with all of its contects!!
+                return user;
+            }
+            else
+            {
+
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+                return null;
+            }
+        }
+
+
         //[Route("Login")]
         //[HttpGet]
         //public UserAccount Login([FromQuery] string email, [FromQuery] string Upass)
--- FootyPunditsBL/ModelsBL/FootyPunditsDBContext.cs
+++ FootyPunditsBL/ModelsBL/FootyPunditsDBContext.cs
@@ -9,5 +9,12 @@ namespace FootyPunditsBL.Models
     public partial class FootyPunditsDBContext
     {
         //scaffold-dbcontext "Server=localhost\sqlexpress;Database=FootyPunditsDB;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -force
+        public UserAccount Login(string email, string pass)
+        {
+            UserAccount user = this.UserAccounts
+                .Where(u => u.Email == email && u.Upass == pass).FirstOrDefault();
+
+            return user;
+        }
     }
 }
